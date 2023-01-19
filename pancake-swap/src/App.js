d6226a1587ce09d15d96cc387b3fdbf9570f1e05
Fatih <kilic8118@gmail.com>
2023-01-19 13:18:15 +0300

    initial react transformation with some issues

--- pancake-swap/src/App.js
+++ pancake-swap/src/App.js
@@ -3,16 +3,25 @@ import Footer from "./Footer";
 import Pottery from "./Pottery";
 import Overview from "./Overview";
 import Perpetual from "./Perpetual";
-
+import Swap from "./Swap";
+import Limit from "./Limit";
+import Bridge from "./Bridge";
+import Ifo from "./Ifo";
+import Home from "./Home";
 import "./stylesheets/index.scss";
 
 function App() {
   return (
     <div className="App">
       {/* <Pottery /> */}
-      <Overview />
+{/*       <Overview /> */}
       {/* <Perpetual /> */}
-      <Footer />
+      <Swap />
+{/*       <Footer /> */}
+      <Limit />
+      <Bridge />
+      <Ifo /> 
+      <Home />
     </div>
   );
 }
