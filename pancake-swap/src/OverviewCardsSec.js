a9868e4086e8bec3c1095307195619f4630dccd9
Gunes Akgun <mgunesak@gmail.com>
2023-01-23 16:32:41 +0300

    search&filter overview

--- pancake-swap/src/OverviewCardsSec.js
+++ pancake-swap/src/OverviewCardsSec.js
@@ -1,6 +1,46 @@
 import React from "react";
 
 const OverviewCardsSec = (props) => {
+  if (props.keyWord.length > 0) {
+    const searchedItem = props.arraySec.filter((el) =>
+      el.headerMain.includes(props.keyWord)
+    );
+    return searchedItem.map((el) => (
+      <div class="overview-newest-arrivals-main-card">
+        <div class="overview-newest-arrivals-main-card-up">
+          <div class="overview-newest-arrivals-main-card-up-row1 banner-ct">
+            <img src={`${el.img}`} alt="banner" class="banner-img-2" />
+          </div>
+          <div class="overview-newest-arrivals-main-card-up-row2">
+            {el.headerTop}
+          </div>
+          <div class="overview-newest-arrivals-main-card-up-row3">
+            {el.headerMain}
+          </div>
+        </div>
+        <div class="overview-newest-arrivals-main-card-down">
+          <div class="overview-newest-arrivals-main-card-down-left">
+            Asking price
+          </div>
+          <div class="overview-newest-arrivals-main-card-down-right">
+            <div class="overview-newest-arrivals-main-card-down-right-1">
+              ${el.priceNum}
+            </div>
+            <div class="overview-newest-arrivals-main-card-down-right-2">
+              <img
+                src={require("./assets/overview/icon-lower-part.svg").default}
+                alt="icon"
+                class="icon-img-2"
+              />
+            </div>
+            <div class="overview-newest-arrivals-main-card-down-right-3">
+              {el.coinNum}
+            </div>
+          </div>
+        </div>
+      </div>
+    ));
+  }
   return props.arraySec.map((el) => (
     <div class="overview-newest-arrivals-main-card">
       <div class="overview-newest-arrivals-main-card-up">
